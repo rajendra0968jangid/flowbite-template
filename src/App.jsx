7527c6139ed61020112da811bbe1548653ec37d2
rajendra0968jangid <rajendra0968jangid@gmail.com>
2024-11-07 18:54:56 +0530

    add more

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-import { Button, Navbar } from "flowbite-react";
+import { Button, Card, Carousel, Footer, Navbar } from "flowbite-react";
+import { BsDribbble, BsFacebook, BsGithub, BsInstagram, BsTwitter } from "react-icons/bs";
 function App() {
 
   return (
@@ -23,6 +24,124 @@ function App() {
           <Navbar.Link href="#">Contact</Navbar.Link>
         </Navbar.Collapse>
       </Navbar>
+      <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+        <Carousel slide={false}>
+          <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
+          <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
+          <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
+          <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
+          <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+        </Carousel>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 m-4">
+        <div>
+          <Card
+            className="max-w-sm"
+            imgAlt="Meaningful alt text for an image that is not purely decorative"
+            imgSrc="/images/blog/image-1.jpg"
+          >
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Noteworthy technology acquisitions 2021
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+            </p>
+          </Card>
+        </div>
+        <div>
+          <Card
+            className="max-w-sm"
+            imgAlt="Meaningful alt text for an image that is not purely decorative"
+            imgSrc="/images/blog/image-1.jpg"
+          >
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Noteworthy technology acquisitions 2021
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+            </p>
+          </Card>
+        </div>
+        <div>
+          <Card
+            className="max-w-sm"
+            imgAlt="Meaningful alt text for an image that is not purely decorative"
+            imgSrc="/images/blog/image-1.jpg"
+          >
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Noteworthy technology acquisitions 2021
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+            </p>
+          </Card>
+        </div>
+        <div>
+          <Card
+            className="max-w-sm"
+            imgAlt="Meaningful alt text for an image that is not purely decorative"
+            imgSrc="/images/blog/image-1.jpg"
+          >
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Noteworthy technology acquisitions 2021
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+            </p>
+          </Card>
+        </div>
+      </div>
+      <Footer bgDark>
+        <div className="w-full">
+          <div className="grid w-full grid-cols-2 gap-8 px-6 py-8 md:grid-cols-4">
+            <div>
+              <Footer.Title title="Company" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">About</Footer.Link>
+                <Footer.Link href="#">Careers</Footer.Link>
+                <Footer.Link href="#">Brand Center</Footer.Link>
+                <Footer.Link href="#">Blog</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="help center" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">Discord Server</Footer.Link>
+                <Footer.Link href="#">Twitter</Footer.Link>
+                <Footer.Link href="#">Facebook</Footer.Link>
+                <Footer.Link href="#">Contact Us</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="legal" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">Privacy Policy</Footer.Link>
+                <Footer.Link href="#">Licensing</Footer.Link>
+                <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="download" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">iOS</Footer.Link>
+                <Footer.Link href="#">Android</Footer.Link>
+                <Footer.Link href="#">Windows</Footer.Link>
+                <Footer.Link href="#">MacOS</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+          <div className="w-full bg-gray-700 px-4 py-6 sm:flex sm:items-center sm:justify-between">
+            <Footer.Copyright href="#" by="Flowbite™" year={2022} />
+            <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
+              <Footer.Icon href="#" icon={BsFacebook} />
+              <Footer.Icon href="#" icon={BsInstagram} />
+              <Footer.Icon href="#" icon={BsTwitter} />
+              <Footer.Icon href="#" icon={BsGithub} />
+              <Footer.Icon href="#" icon={BsDribbble} />
+            </div>
+          </div>
+        </div>
+      </Footer>
     </>
   )
 }
